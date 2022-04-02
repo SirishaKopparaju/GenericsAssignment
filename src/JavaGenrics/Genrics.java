0f3d233e61a600519dec5571e5c2eba9bdf2a6a8
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-04-02 17:45:21 +0530

    refactor:largest of 3 numbers

--- src/JavaGenrics/Genrics.java
+++ src/JavaGenrics/Genrics.java
@@ -5,19 +5,16 @@ public class Genrics<T> {
 	    T max = x; // assume x is initially the largest
 
 	    if (y.compareTo(max) > 0)
-	      max = y; // y is the largest so far
+	      max = y;
 
 	    if (z.compareTo(max) > 0)
-	      max = z; // z is the largest
+	      max = z; 
 
-	    return max; // returns the largest object
-	  } // end method maximum
+	    return max; 
+	  } 
 
 	  public static void main(String args[]) {
 	    System.out.printf("Maximum of %d, %d and %d is %d\n\n", 3, 4, 5, maximum(3, 4, 5));
-	    System.out.printf("Maximum of %.1f, %.1f and %.1f is %.1f\n\n", 6.6, 8.8, 7.7, maximum(6.6,
-	        8.8, 7.7));
-	    System.out.printf("Maximum of %s, %s and %s is %s\n", "pear", "apple", "orange", maximum(
-	        "pear", "apple", "orange"));
+	    
 	  }
 }
