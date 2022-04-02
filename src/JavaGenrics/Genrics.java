064bafa0b98eff52cfb50086e9a38c195db5178a
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-04-02 17:48:24 +0530

    refactor largest of 3 strings

--- src/JavaGenrics/Genrics.java
+++ src/JavaGenrics/Genrics.java
@@ -14,8 +14,8 @@ public class Genrics<T> {
 	  } 
 
 	  public static void main(String args[]) {
-		    System.out.printf("Maximum of %.1f, %.1f and %.1f is %.1f\n\n", 6.6, 8.8, 7.7, maximum(6.6,
-		            8.8, 7.7));
+		    System.out.printf("Maximum of %s, %s and %s is %s\n", "pear", "apple", "orange", maximum(
+		            "pear", "apple", "orange"));
 	    
 	  }
 }
