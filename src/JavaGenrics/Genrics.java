e539d33e4ad040b57a0db9fe36676511ab838af0
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-04-02 16:33:13 +0530

    added:using generics

--- src/JavaGenrics/Genrics.java
+++ src/JavaGenrics/Genrics.java
@@ -1,39 +1,24 @@
 package JavaGenrics;
 
 public class Genrics {
-private static Genrics PrintArray;
-public static void toPrint(Integer[] inputArray) {
-	for(int element:inputArray) {
+
+public static <E> void toPrint(E[] inputArray) {
+	for(E element:inputArray) {
 		System.out.printf("%s",element);
 		
 	}
 	System.out.println();
 }
-public static void toPrint(double[] inputArray) {
-	for(double element:inputArray) {
-		System.out.printf("%s",element);
-		
-	}
-	System.out.println();
-}
-public static void toPrint(Character[] inputArray) {
-	for(char element: inputArray) {
-		System.out.printf("%s",element);
-		
-	}
-	System.out.println();
-}
+
 public static void main(String[]args) {
 	Integer[] intArray = {1,2,3,4,5} ;
-	double[] doubleArray = {1.1,2.2,3.3,4.4};
-	char[] charArray = {'H','E','L','L','O'};
-	PrintArray.toPrint(intArray);
-	PrintArray.toPrint(doubleArray);
+	Double[] doubleArray = {1.1,2.2,3.3,4.4};
+	Character[] charArray = {'H','E','L','L','O'};
+	Genrics.toPrint(intArray);
+	Genrics.toPrint(doubleArray);
+	Genrics.toPrint(charArray);
 
-	
-	
+
 	
 }
-
-
 }
