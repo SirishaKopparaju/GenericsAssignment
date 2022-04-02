e3422b83ecce05ba8022d030048b89f0fc06e3d9
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-04-02 17:42:43 +0530

    added:maximum of 3 numbers using generics

--- src/JavaGenrics/Genrics.java
+++ src/JavaGenrics/Genrics.java
@@ -1,31 +1,23 @@
 package JavaGenrics;
 
 public class Genrics<T> {
-	private T[] inputArray;
-	public Genrics(T[] inputArray) {
-		this.inputArray=inputArray;
-	}
-	public void toPrint() {
-		Genrics.toPrint(this.inputArray);
-	}
+	public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
+	    T max = x; // assume x is initially the largest
 
-public static <E> void toPrint(E[] inputArray) {
-	for(E element:inputArray) {
-		System.out.printf("%s",element);
-		
-	}
-	System.out.println();
-}
+	    if (y.compareTo(max) > 0)
+	      max = y; // y is the largest so far
 
-public static void main(String[]args) {
-	Integer[] intArray = {1,2,3,4,5} ;
-	Double[] doubleArray = {1.1,2.2,3.3,4.4};
-	Character[] charArray = {'H','E','L','L','O'};
-	new Genrics(intArray).toPrint();
-	Genrics.toPrint(doubleArray);
-	Genrics.toPrint(charArray);
+	    if (z.compareTo(max) > 0)
+	      max = z; // z is the largest
 
+	    return max; // returns the largest object
+	  } // end method maximum
 
-	
-}
+	  public static void main(String args[]) {
+	    System.out.printf("Maximum of %d, %d and %d is %d\n\n", 3, 4, 5, maximum(3, 4, 5));
+	    System.out.printf("Maximum of %.1f, %.1f and %.1f is %.1f\n\n", 6.6, 8.8, 7.7, maximum(6.6,
+	        8.8, 7.7));
+	    System.out.printf("Maximum of %s, %s and %s is %s\n", "pear", "apple", "orange", maximum(
+	        "pear", "apple", "orange"));
+	  }
 }
